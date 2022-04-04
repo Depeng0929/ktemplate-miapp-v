aa7ad1513ab031dad40d08bcde8a31eed94b9d11
depeng.kong <15365182986@163.com>
2022-04-04 23:44:49 +0800

    declare

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -1,40 +1,29 @@
-export const plathEnum = {
-  weixin: 'mp',
-  alipay: 'alipay',
-  h5: 'h5',
+enum PLATHFORM {
+  WX = 'weixin',
+  ALIPAY = 'alipay',
+  H5 = 'h5',
 }
 
-function platform() {
+function platform(): PLATHFORM {
   let result
 
   // #ifdef  MP-WEIXIN
-  result = plathEnum.weixin
+  result = PLATHFORM.WX
   // #endif
 
   // #ifdef  MP-ALIPAY
-  result = plathEnum.alipay
+  result = PLATHFORM.ALIPAY
   // #endif
 
   // #ifdef  H5
-  result = plathEnum.h5
+  result = PLATHFORM.H5
   // #endif
 
   return result
 }
 
-export const isWeixin = platform() === plathEnum.weixin
-export const isAlipay = platform() === plathEnum.alipay
-export const isH5 = platform() === plathEnum.h5
+export const isWeixin = platform() === PLATHFORM.WX
+export const isAlipay = platform() === PLATHFORM.ALIPAY
+export const isH5 = platform() === PLATHFORM.H5
 
 export const isDev = process.env.NODE_ENV === 'development'
-
-// global env config
-export const getConfig = () => {
-  const { VUE_APP_API, VUE_APP_IMG, VUE_APP_STATIC_IMG, VUE_APP_API_CENTER } = process.env
-  return {
-    api: VUE_APP_API,
-    api_center: VUE_APP_API_CENTER,
-    staticURL: VUE_APP_STATIC_IMG,
-    imageURL: VUE_APP_IMG,
-  }
-}
