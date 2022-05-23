396b43830a5d75edb56af14daac4f41ff5f1dcf2
depeng.kong <15365182986@163.com>
2022-05-23 23:16:01 +0800

    docs: add debonce button docs

--- src/composables/button.ts
+++ src/composables/button.ts
@@ -1,5 +1,22 @@
 import { debounce } from '@depeng9527/tools'
 
+/**
+ * debonce button hooks
+ *
+ * @param promiseFn - The function that return promise
+ * @returns { status: { loading: boolean, disabled: boolean }, onClick: () => void }
+ * @example
+ * ```
+ * function onClick() {
+ *  return new Promise((resolve) => {
+ *    setTimeout(() => {
+ *      resolve(1)
+ *    }, 2000)
+ *  })
+ * }
+ * const submit = reactive(useButton(onClick))
+ * ```
+ */
 export function useButton(promiseFn: (...args: unknown[]) => Promise<unknown>) {
   const status = ref({
     loading: false,
