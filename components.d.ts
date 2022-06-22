ee48ec6cdee264862d0dada6d160d5fba9d04367
depeng.kong <15365182986@163.com>
2022-06-22 18:14:26 +0800

    chore: update deps

--- components.d.ts
+++ components.d.ts
@@ -5,6 +5,7 @@ import '@vue/runtime-core'
 
 declare module '@vue/runtime-core' {
   export interface GlobalComponents {
+    AppButton: typeof import('./src/components/AppButton.vue')['default']
     List: typeof import('./src/components/List.vue')['default']
     Loading: typeof import('./src/components/Loading.vue')['default']
   }
