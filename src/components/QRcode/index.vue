78a6c41ec9391104ab7b20723a6096c05f8d60aa
depeng.kong <15365182986@163.com>
2022-07-11 23:26:21 +0800

    fix: alipay code

--- src/components/QRcode/index.vue
+++ src/components/QRcode/index.vue
@@ -1,17 +1,45 @@
 <script setup lang='ts'>
-const props = defineProps<{
-  code: string
-  size?: number
-}>()
-
-const config = reactive({
-  code: props.code,
-  size: props.size ?? 460,
+import UQRCode from 'u-qrcode'
+const props = defineProps({
+  id: {
+    type: String,
+    default: 'qrcode',
+  },
+  code: {
+    type: String,
+    default: '',
+  },
+  size: {
+    type: Number,
+    default: 0,
+  },
 })
+const sizePx = $computed(() => {
+  return uni.upx2px(props.size)
+})
+
+watch(() => props.code, (val) => {
+  if (val)
+    nextTick(() => change(val))
+}, { immediate: true })
+
+function change(val: string) {
+  const ctx = uni.createCanvasContext(props.id)
+  const uqrcode: any = new UQRCode(
+    {
+      text: val,
+      size: sizePx,
+    },
+    ctx,
+  )
+  uqrcode.make()
+  uqrcode.draw()
+}
+
 </script>
 
 <template>
-  <view class="mx-auto" :style="{width: `${config.size}rpx`, height: `${config.size}rpx`}">
-    <w-qrcode :options="config" />
+  <view class="mx-auto" :style="{width: `${sizePx}px`, height: `${sizePx}px`}">
+    <canvas id="qrcode" canvas-id="qrcode" :style="{ width: `${sizePx}px`, height: `${sizePx}px` }" />
   </view>
 </template>
