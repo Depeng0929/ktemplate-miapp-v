56c18e035cdc9acb93a737c84431ea02e8329692
depeng.kong <15365182986@163.com>
2022-07-11 22:17:08 +0800

    fix: alipay Reader

--- src/components/Reader.vue
+++ src/components/Reader.vue
@@ -1,22 +1,42 @@
 <script setup lang='ts'>
 import mpHtml from 'mp-html/dist/uni-app/components/mp-html/mp-html.vue'
+import { isAlipay } from '~/utils'
+
+const ROOT_REM = 14
+
 const props = withDefaults(defineProps<{
   html?: string
 }>(), {
   html: '',
 })
 
+const processedHtml = $computed(() => {
+  return process(props.html)
+})
+
+function process(str: string) {
+  return transformCssRem(str)
+
+  function transformCssRem(str: string) {
+    if (isAlipay) return str
+
+    return str.replace(/(\d+(\.\d{1,2})?)rem/mg, (matchs, m1) => {
+      return `${m1 * ROOT_REM}px`
+    })
+  }
+}
 </script>
 
 <template>
   <view class="app-reader">
-    <mp-html :content="props.html" />
+    <mp-html :content="processedHtml" />
   </view>
 </template>
 
 <style lang="scss">
 .app-reader {
   font-weight: 300;
+  word-break: break-all;
   rich-text {
     text-indent: 2em;
   }
