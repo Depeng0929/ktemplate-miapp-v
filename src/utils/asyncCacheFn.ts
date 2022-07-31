518fcbb16becb4875cb1b073f14f2c498283bcbe
depeng.kong <15365182986@163.com>
2022-07-31 20:01:33 +0800

    fix: asyncache noramlly

--- src/utils/asyncCacheFn.ts
+++ src/utils/asyncCacheFn.ts
@@ -1,5 +1,4 @@
 import { LRU } from '@depeng9527/k-data'
-import type { Ref, ShallowRef } from 'vue'
 
 export function asyncCacheFn<R, A extends unknown[], K = string>(
   fn: (...args: A) => Promise<R>,
@@ -12,38 +11,25 @@ export function asyncCacheFn<R, A extends unknown[], K = string>(
   const lru = new LRU<R, K | string>()
   const pendding = new Set<Promise<R>>()
 
-  const wrapper = ((...args: A) => {
-    const data = shallowRef<R | null>(null)
-    const loadding = ref(false)
-    const error = shallowRef(null)
-
+  const wrapper = (...args: A) => {
     const key = getKey(args)
 
     if (lru.has(key))
-      data.value = lru.get(key)!
+      return lru.get(key)!
 
     const promise = Promise.resolve(fn(...args))
 
-    loadding.value = true
     promise.then((res) => {
-      data.value = res
       lru.add(key, res)
-    }).catch((err) => {
-      error.value = err
+      return res
     }).finally(() => {
-      loadding.value = false
       pendding.delete(promise)
     })
 
     pendding.add(promise)
 
-    return {
-      data,
-      loadding,
-      error,
-    }
-  }) as asyncFunction<A>
-  // https://blog.csdn.net/weixin_43477825/article/details/119912771
+    return promise
+  }
   wrapper.clear = () => lru.clear()
 
   return wrapper
@@ -51,13 +37,4 @@ export function asyncCacheFn<R, A extends unknown[], K = string>(
 
 interface AsyncCacheFnOptions<A extends any[], K = string> {
   getKey?: (args: A) => K
-}
-
-interface asyncFunction<A extends any[]> {
-  <T = unknown>(...args: A): {
-    data: ShallowRef<T>
-    loadding: Ref<boolean>
-    error: ShallowRef<any>
-  }
-  clear: () => void
 }
