5621aebc927f3dcb5a38db2162f97ba13b1291bf
depeng.kong <15365182986@163.com>
2022-08-17 16:17:44 +0800

    style

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,7 @@
+import type { InjectionKey, Ref } from 'vue'
 import config from '~/config.json';
 
 export const STORAGE_KEY = `__${config.project_name}_token__`
+
+// provider example
+export const injectionClicks: InjectionKey<Ref<number>> = Symbol('v-click-clicks')
