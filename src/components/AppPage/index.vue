02aa1a957d6e615967dc02ad8d3c6c79ecf1685d
depeng.kong <15365182986@163.com>
2022-09-07 16:45:56 +0800

    feat(h5): support full page

--- src/components/AppPage/index.vue
+++ src/components/AppPage/index.vue
@@ -1,5 +1,21 @@
+<script lang="ts" setup>
+import { isH5 } from '~/utils/index'
+
+const pageStyles = $computed(() => {
+  if (isH5) {
+    return {
+      minHeight: 'calc(100vh-44px)',
+    }
+  }
+
+  return {
+    minHeight: '100vh',
+  }
+})
+</script>
+
 <template>
-  <div class="min-h-screen app-page">
+  <div class="app-page" :style="pageStyles">
     <Auth>
       <Preload>
         <slot />
