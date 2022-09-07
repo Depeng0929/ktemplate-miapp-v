e2a446d3697ef8904aaa82f27d6989587d546126
depeng.kong <15365182986@163.com>
2022-09-07 23:23:49 +0800

    fix: appage style

--- src/components/AppPage/index.vue
+++ src/components/AppPage/index.vue
@@ -4,18 +4,18 @@ import { isH5 } from '~/utils/index'
 const pageStyles = $computed(() => {
   if (isH5) {
     return {
-      minHeight: 'calc(100vh-44px)',
+      minHeight: 'calc(100vh - 44px)',
     }
   }
 
   return {
-    minHeight: '100vh',
+    minHeight: 'calc(100vh - 4px)',
   }
 })
 </script>
 
 <template>
-  <div class="app-page" :style="pageStyles">
+  <div class="app-page border-top !border-transparent" :style="pageStyles">
     <Auth>
       <Preload>
         <slot />
@@ -23,3 +23,4 @@ const pageStyles = $computed(() => {
     </Auth>
   </div>
 </template>
+
