2042a5990f411e26fcdf9129e32a51b6d3f631ca
depeng.kong <15365182986@163.com>
2022-09-07 21:12:29 +0800

    feat: Avatar components

--- src/components/Avatar.vue
+++ src/components/Avatar.vue
@@ -1,22 +1,36 @@
 <script setup lang="ts">
 const {
+  size = 80,
   src = '',
-  size = 40,
 } = defineProps<{
   size?: number
-  src?: string
+  src?: `/${string}` | string
 }>()
 
-const warpStyle = $computed(() => {
+const themeColor = '#777777'
+
+const isDefault = $computed(() => {
+  return !src
+})
+const upxSize = $computed(() => {
+  return uni.upx2px(size)
+})
+const warpStyles = $computed(() => {
+  const n = `${upxSize}px`
   return {
-    width: `${size}px`,
-    height: `${size}px`,
+    with: n,
+    height: n,
   }
 })
 </script>
 
 <template>
-  <view :style="warpStyle">
-    <AppImage :src="src" :width="size" :height="size" />
+  <view
+    :style="warpStyles"
+    class="rounded-[50%] inline-block overflow-hidden border border-2 "
+    :class="[isDefault ? '!border-[#777777]' : '!border-transparent']"
+  >
+    <uni-icons v-if="isDefault" type="person-filled" :size="upxSize" :color="themeColor" />
+    <AppImage v-else :src="src" :width="size" :height="size" />
   </view>
 </template>
